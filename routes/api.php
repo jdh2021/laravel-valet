d891464c79778bb3a2b7375fa7f666d4f584e611
Jennifer Hightower <jenniferhightower2020@gmail.com>
2023-01-31 11:01:47 -0600

    created listings (view). revised GET route in web.php to pass array into listings (view). added foreach to loop over each listing in listings.

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/posts', function() {
+//   // return JSON for building API, use response helper
+//   return response()->json([
+//     'posts' => [
+//         'title' => 'Post One'
+//     ]
+//     ]);
+// });
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
--- routes/web.php
+++ routes/web.php
@@ -16,26 +16,42 @@ use Illuminate\Support\Facades\Route;
 
 // route class to call HTTP method GET. similar to express.js. takes in endpoint and closure that is a function.
 Route::get('/', function () {
-    return view('welcome');
+    // pass data that is array
+    return view('listings', [
+        'heading' => 'Latest Listings',
+        // passing array into listings View
+        'listings' => [
+            [
+                'id' => 1,
+                'title' => 'Listing One',
+                'description' => 'Paint small barn'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Listing Two',
+                'description' => 'Mount TV in living room'
+            ],
+
+        ]
+    ]);
 });
 
-//
-Route::get('/hello', function() {
-    // response helper. takes in string and status. add headers.
-    return response('<h1>Hello World</h1>', 200) 
-        ->header('Content-Type', 'text/plain');
-});
+// Route::get('/hello', function() {
+//     // response helper. takes in string and status. add headers.
+//     return response('<h1>Hello World</h1>', 200) 
+//         ->header('Content-Type', 'text/plain');
+// });
 
-// route with wildcard, curly braces around param for wildcard. closure is function with variable of same name
-Route::get('/posts/{id}', function($id) {
-    // debugging, showing value of what is passed in with helper methods - dump, die, debug
-    // ddd($id);
-    return response ('Post '. $id);
-    // add constraints to what can be passed. 
-    })->where('id', '[0-9]+');
+// // route with wildcard, curly braces around param for wildcard. closure is function with variable of same name
+// Route::get('/posts/{id}', function($id) {
+//     // debugging, showing value of what is passed in with helper methods - dump, die, debug
+//     // ddd($id);
+//     return response ('Post '. $id);
+//     // add constraints to what can be passed. 
+//     })->where('id', '[0-9]+');
 
-// route with request, query params, import class
-Route::get('/search', function(Request $request) {
-    return $request->name . ' ' . $request->city;
-});
+// // route with request, query params, import class
+// Route::get('/search', function(Request $request) {
+//     return $request->name . ' ' . $request->city;
+// });
 
