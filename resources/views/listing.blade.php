b41e3552d24e8df43ed8835f389c5904924bbf94
Jennifer Hightower <jenniferhightower2020@gmail.com>
2023-02-03 11:05:22 -0600

    created components folder. created listing-card.blade.php and card.blade.php. listing card component takes in prop. updated listings.blade.php with <x-listing-card>. added attributes to card component with ->merge

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -8,7 +8,7 @@
 
 <a href="/" class="inline-block text-black ml-4 mb-4"><i class="fa-solid fa-arrow-left"></i> Back</a>
 <div class="mx-4">
-    <div class="bg-gray-50 border border-gray-200 p-10 rounded">
+    <x-card class="p-24">
         <div
             class="flex flex-col items-center justify-center text-center"
         >
@@ -68,7 +68,7 @@
                 </div>
             </div>
         </div>
-    </div>
+    </x-card>
 </div>
 
 @endsection
--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -25,48 +25,8 @@
 
 <!-- foreach directive in Blade -->
 @foreach($listings as $listing)
-<div class="bg-gray-50 border border-gray-200 rounded p-6">
-    <div class="flex">
-        <!-- asset helper to show public images -->
-        <img
-            class="hidden w-48 mr-6 md:block"
-            src="{{asset('images/no-image.png')}}"
-            alt=""
-        />
-        <div>
-            <h3 class="text-2xl">
-                <!-- Eloquent models give collection. $listing['title'] replaced by $listing->title-->
-                <a href="/listings/{{$listing->id}}">{{$listing->title}}</a>
-            </h3>
-            <div class="text-xl font-bold mb-4">{{$listing->company}}</div>
-            <ul class="flex">
-                <li
-                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
-                >
-                    <a href="#">Laravel</a>
-                </li>
-                <li
-                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
-                >
-                    <a href="#">API</a>
-                </li>
-                <li
-                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
-                >
-                    <a href="#">Backend</a>
-                </li>
-                <li
-                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
-                >
-                    <a href="#">Vue</a>
-                </li>
-            </ul>
-            <div class="text-lg mt-4">
-                <i class="fa-solid fa-location-dot"></i> {{$listing->location}}
-            </div>
-        </div>
-    </div>
-</div>
+    <!-- access listing card component, bind variable to prop with : -->
+    <x-listing-card :listing="$listing"/>
 @endforeach
 
 @else 
