cd5521c30c9d4c249f083238c33d3c10d260a03b
Jennifer Hightower <jenniferhightower2020@gmail.com>
2023-02-09 18:53:32 -0600

    added alpine.js as minimal js framework to layout component. within flash-message.blade.php view, added x-data and x-init with setTimeout to specify how long flash message will display once triggered when form is successfully submitted

--- resources/views/components/flash-message.blade.php
+++ resources/views/components/flash-message.blade.php
@@ -1,6 +1,10 @@
 
 @if(session()->has('message'))
-    <div class="fixed top-0 left-1/2 transform -translate-x-1/2 bg-laravel text-white px-48 py">
+    <!-- when message is initialized, disappear after certain amount of time w/setTimeout. setTimeout takes in arrow function where show is equal to false after 2 seconds -->
+    <div x-data="{show: true}" 
+        x-init="setTimeout(()=> show = false, 2000)"  
+        x-show="show" 
+        class="fixed top-0 left-1/2 transform -translate-x-1/2 bg-laravel text-white px-48 py">
         <p>
             {{session('message')}}
         </p>
