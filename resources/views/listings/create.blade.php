ca76036e335af3b42293cd856d11ce3278eaa3e3
Jennifer Hightower <jenniferhightower2020@gmail.com>
2023-02-10 19:39:42 -0600

    revised form inputs in create.blade.php to include value with old() helper and name of field passed in. old helper preserves info already input in fields when validate error is encountered upon form submission

--- resources/views/listings/create.blade.php
+++ resources/views/listings/create.blade.php
@@ -21,6 +21,7 @@
             type="text"
             class="border border-gray-200 rounded p-2 w-full"
             name="company"
+            value="{{old('company')}}"
         />
 
         <!-- error directive if company field fails validation -->
@@ -30,7 +31,8 @@
     </div>
 
     <div class="mb-6">
-        <label for="title" class="inline-block text-lg mb-2"
+        <label 
+            for="title" class="inline-block text-lg mb-2"
             >Job Title</label
         >
         <input
@@ -38,6 +40,7 @@
             class="border border-gray-200 rounded p-2 w-full"
             name="title"
             placeholder="Example: Senior Laravel Developer"
+            value="{{old('title')}}"
         />
         @error('title')
             <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -55,6 +58,7 @@
             class="border border-gray-200 rounded p-2 w-full"
             name="location"
             placeholder="Example: Remote, Boston MA, etc"
+            value="{{old('location')}}"
         />
         @error('location')
             <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -69,6 +73,7 @@
             type="text"
             class="border border-gray-200 rounded p-2 w-full"
             name="email"
+            value="{{old('email')}}"
         />
         @error('email')
             <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -86,6 +91,7 @@
             type="text"
             class="border border-gray-200 rounded p-2 w-full"
             name="website"
+            value="{{old('website')}}"
         />
         @error('website')
             <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -101,6 +107,7 @@
             class="border border-gray-200 rounded p-2 w-full"
             name="tags"
             placeholder="Example: Laravel, Backend, Postgres, etc"
+            value="{{old('tags')}}"
         />
         @error('tags')
             <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -130,6 +137,7 @@
             name="description"
             rows="10"
             placeholder="Include tasks, requirements, salary, etc"
+            {{old('description')}}
         ></textarea>
         @error('description')
             <p class="text-red-500 text-xs mt-1">{{$message}}</p>
