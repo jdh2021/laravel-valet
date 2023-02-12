d6d2bef818e5df94b640c1781785dcb0bcca698d
Jennifer Hightower <jenniferhightower2020@gmail.com>
2023-02-12 12:40:13 -0600

    added symlink to link images from storage/app/public folder to public folder using php artisan storage:link. updated img src attribute in listing-card.blade.php and show.blade.php (single listing view) with ternary that checks if storage/listinglogolink is true. if not, the generic laravel logo is shown

--- resources/views/components/listing-card.blade.php
+++ resources/views/components/listing-card.blade.php
@@ -4,10 +4,10 @@
 
 <x-card>
     <div class="flex">
-        <!-- asset helper to show public images -->
+        <!-- asset helper to show public images, ternary to check if listing logo avail -->
         <img
             class="hidden w-48 mr-6 md:block"
-            src="{{asset('images/no-image.png')}}"
+            src="{{$listing->logo ? asset('storage/' . $listing->logo) : asset('/images/no-image.png')}}"
             alt=""
         />
         <div>
