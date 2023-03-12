375524302cf832ed6b0ef4c77db25c97e2d13704
Jennifer Hightower <jenniferhightower2020@gmail.com>
2023-03-11 21:15:57 -0600

    created manage.blade.php and added html from tutorial to return view from manage method in ListingController. used @foreach, @endforeach to loop through user's listings and display each in table row. used @unless, @endunless to display message when user does not have any job postings.

---
+++ resources/views/listings/manage.blade.php
@@ -0,0 +1,58 @@
+<x-layout>
+    <x-card class="p-10">
+        <header>
+            <h1
+                class="text-3xl text-center font-bold my-6 uppercase"
+            >
+                Manage Gigs
+            </h1>
+        </header>
+        <table class="w-full table-auto rounded-sm">
+            <tbody>
+                <!-- loop through the listings that logged in user has -->
+                @unless($listings->isEmpty())
+                @foreach($listings as $listing)
+                <tr class="border-gray-300">
+                    <td
+                        class="px-4 py-8 border-t border-b border-gray-300 text-lg"
+                    >
+                        <a href="show.html">
+                            {{$listing->title}}
+                        </a>
+                    </td>
+                    <td
+                        class="px-4 py-8 border-t border-b border-gray-300 text-lg"
+                    >
+                        <a
+                            href="/listings/{{$listing->id}}/edit"
+                            class="text-blue-400 px-6 py-2 rounded-xl"
+                            ><i
+                                class="fa-solid fa-pen-to-square"
+                            ></i>
+                            Edit</a
+                        >
+                    </td>
+                    <td
+                        class="px-4 py-8 border-t border-b border-gray-300 text-lg"
+                    >
+                    <form method="POST" action="/listings/{{$listing->id}}">
+                        <!-- cross-site protection -->
+                        @csrf
+                        @method('DELETE')
+                        <button class="text-red-500"><i class="fa-solid fa-trash"></i>Delete</button>
+                    </form>
+                    </td>
+                </tr>
+                @endforeach
+                <!-- if there are no listings to loop through-->
+                @else
+                <tr class="border-gray-300">
+                    <td class="px-4 py-8 border-t border-b border-gray-300 tx-lg">
+                        <p class="text-center">No listings found</p>
+                    </td>
+                </tr>
+                @endunless
+            </tbody>
+        </table>
+    </x-card>
+</x-layout>
